78c84f30f4c85312f96e042a3ba33dd6bbce44a3
zaki-lknr <zaki.hmkc+github@gmail.com>
2025-07-13 21:44:48 +0900

    add: 入出力ファイルのオプション指定

--- swarm/marge-log.py
+++ swarm/marge-log.py
@@ -1,5 +1,6 @@
 import json
 import datetime
+import argparse
 
 new_data_file = 'logs/today.json'
 stored_data_file = 'logs/all-checkins.json'
@@ -76,9 +77,19 @@ def statistics(checkins):
     return(data)
 
 if __name__ == "__main__":
-    checkins = get_marge_data(stored_data_file, new_data_file)
+    p = argparse.ArgumentParser()
+    p.add_argument("--base", help="base file")
+    p.add_argument("--add", help="add file")
+    p.add_argument("--out", help="output file")
+    args = p.parse_args()
+
+    base_file = args.base or stored_data_file
+    add_file = args.add or new_data_file
+    output_file = args.out or new_stored_data_file
+
+    checkins = get_marge_data(base_file, add_file)
     data = statistics(checkins)
-    with open(new_stored_data_file, mode='w') as f:
+    with open(output_file, mode='w') as f:
         f.write(json.dumps(checkins, ensure_ascii=False))
 
     with open(statistics_file, mode='w') as f:
