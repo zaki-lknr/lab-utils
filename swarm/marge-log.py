23fe07e08545e464ae549cb678bb0c4b136a1037
zaki-lknr <zaki.hmkc+github@gmail.com>
2025-07-19 09:05:40 +0900

    update: 統計はcalcへ移動

--- swarm/marge-log.py
+++ swarm/marge-log.py
@@ -5,7 +5,6 @@ import argparse
 new_data_file = 'logs/today.json'
 stored_data_file = 'logs/all-checkins.json'
 new_stored_data_file = 'logs/all-checkins-current.json'
-statistics_file = 'logs/stat.json'
 # print(files)
 
 def get_marge_data(base_file, add_file):
@@ -35,47 +34,6 @@ def get_marge_data(base_file, add_file):
     # print(json.dumps(all_checkins, ensure_ascii=False))
     return(all_checkins)
 
-def statistics(checkins):
-    data = {
-        'statistics': {},
-        'expired': [],
-    }
-    expired_work = set()
-    current_dt = datetime.datetime.now()
-    limit_sec = int((current_dt + datetime.timedelta(days=-30)).timestamp())
-
-    for checkin in checkins:
-        checkin_id = checkin['venue']['id']
-        checkin_time = datetime.datetime.fromtimestamp(checkin['createdAt'])
-        item = data['statistics'].get(checkin_id)
-        if (limit_sec > checkin['createdAt']):
-            # 期限切れ
-            if (not item):
-                expired_work.add(checkin['venue']['name'])
-            continue
-
-        diff = current_dt - checkin_time
-        lost = str(int((checkin['createdAt'] - limit_sec) / 60 / 60)) + "h"
-        if item:
-            item['count'] += 1
-            item['oldest'] = str(checkin_time)
-            item['lost'] = lost
-            item['checkins'].append((checkin_time).strftime('%m/%d'))
-        else:
-            data['statistics'][checkin_id] = {
-                'count': 1,
-                'name': checkin['venue']['name'],
-                'latest': str(checkin_time),
-                'passed': str(diff.days) + "d, " + str(int(diff.seconds / 60 / 60)) + "h",
-                'oldest': str(checkin_time),
-                'lost': lost,
-                'checkins': [(checkin_time).strftime('%m/%d')],
-            }
-
-    data['statistics'] = dict(sorted(data['statistics'].items(), key=lambda x: x[1]['count'], reverse=True))
-    data['expired'] = list(expired_work)
-    return(data)
-
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
     p.add_argument("--base", help="base file")
@@ -88,9 +46,5 @@ if __name__ == "__main__":
     output_file = args.out or new_stored_data_file
 
     checkins = get_marge_data(base_file, add_file)
-    data = statistics(checkins)
     with open(output_file, mode='w') as f:
         f.write(json.dumps(checkins, ensure_ascii=False))
-
-    with open(statistics_file, mode='w') as f:
-        f.write(json.dumps(data, ensure_ascii=False))
