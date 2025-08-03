439e98b3bc24674f86815157a4f59afc50dd80f6
zaki-lknr <zaki.hmkc+github@gmail.com>
2025-08-03 10:30:54 +0900

    add: 桁揃え

--- swarm/calc.py
+++ swarm/calc.py
@@ -73,9 +73,10 @@ def statistics(src_file, thr_file):
             lost_h = d['lost']
 
             if pass_h >= item['threshold'] or d['count'] <= item['count']:
-                st = 'c:' + str(d['count']) + '(' + str(item['count']) + ')/'
-                st += 'int:' + str(pass_h) + '(' + str(item['threshold']) + ')/'
-                st += 'exp:' + str(lost_h) + '| ' + item['name']
+                st = 'c:{: >2}({: >2})/'.format(d['count'], item['count'])
+                st += 'int:{: >2}({: >2})/'.format(pass_h, item['threshold'])
+                st += 'exp:{: >2}'.format(lost_h)
+                st += '| ' + item['name']
                 # print(st)
                 data['threshold'].append(st)
 
