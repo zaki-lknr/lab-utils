ed317e29a7875eef5f88bc48e601fe5674e367de
zaki-lknr <zaki.hmkc+github@gmail.com>
2025-09-28 12:05:37 +0900

    add: option:出力ファイル

--- swarm/count.py
+++ swarm/count.py
@@ -4,8 +4,10 @@ import argparse
 
 threshold_file = 'threshold.json'
 statistics_file = 'logs/stat.json'
+result_file = 'result.log'
 
 def statistics(stat_file, thr_file):
+    result = []
 
     with open(stat_file) as f:
         stats = json.load(f)
@@ -23,20 +25,23 @@ def statistics(stat_file, thr_file):
             latest = d.strftime('%Y/%m/%d')
             d = datetime.datetime.strptime(checkin_stat['oldest'], '%Y-%m-%d %H:%M:%S')
             oldest = d.strftime('%Y/%m/%d')
-            print("{},{},{},{}".format(name, count, latest, oldest))
+            result.append("{},{},{},{}".format(name, count, latest, oldest))
         else:
-            print("{},{},{},{}".format(name, "", "", ""))
+            result.append("{},{},{},{}".format(name, "", "", ""))
+    return result
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
     p.add_argument("--stat", help="stat file")
     p.add_argument("--threshold", help="threshold configure file")
+    p.add_argument("--out", help="output file")
     args = p.parse_args()
 
     stat_file = args.stat or statistics_file
     thr_file = args.threshold or threshold_file
+    out_file = args.out or result_file
 
-    statistics(stat_file, thr_file)
+    r = statistics(stat_file, thr_file)
 
-    # with open(out_file, mode='w') as f:
-    #     f.write(json.dumps(data, ensure_ascii=False, indent=2))
+    with open(out_file, mode='w') as f:
+        f.write("\n".join(r))
